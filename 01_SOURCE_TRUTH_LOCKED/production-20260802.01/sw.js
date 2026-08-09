f015cec88b90272512e7f9e9cfcb26ed822e1c48
Pipe Blade <gridnode.infra@gmail.com>
2026-08-08 21:32:29 -0400

    chore(lock): re-lock for 20260808.17

--- 01_SOURCE_TRUTH_LOCKED/production-20260802.01/sw.js
+++ 01_SOURCE_TRUTH_LOCKED/production-20260802.01/sw.js
@@ -4,8 +4,8 @@
  */
 'use strict';
 
-const RELEASE = '20260808.16';
-const CACHE_NAME = 'gridnode-shell-20260808-15' + RELEASE.replace(/\./g, '-');
+const RELEASE = '20260808.17';
+const CACHE_NAME = 'gridnode-shell-20260808-17' + RELEASE.replace(/\./g, '-');
 const V = '?v=' + RELEASE;
 const SHELL = [
   '/',
